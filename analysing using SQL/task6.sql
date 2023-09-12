e735edea8f3c212700903432e1e6677bac9598cd
Saibal Patra <102281722+SaibalPatraDS@users.noreply.github.com>
2023-09-12 12:39:11 +0530

    Update task6.sql

--- analysing using SQL/task6.sql
+++ analysing using SQL/task6.sql
@@ -1,1 +1,9 @@
+/* Find the regions with the highest number of nodes assigned to them. */
 
+SELECT w.region_name,
+       COUNT(node_id) AS total_nodes
+FROM world_regions w 
+JOIN user_nodes n  
+ON w.region_id = n.region_id
+GROUP BY region_name
+ORDER BY total_nodes DESC;
